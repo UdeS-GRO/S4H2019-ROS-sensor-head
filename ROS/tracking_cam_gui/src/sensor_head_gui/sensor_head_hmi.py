ac5a7f9a3d17b50bcc5cd81565b430d8626721a0
Michael Simard <michael.simard@usherbrooke.ca>
2019-02-20 10:47:40 -0500

    New fresh repo

---
+++ ROS/tracking_cam_gui/src/sensor_head_gui/sensor_head_hmi.py
@@ -0,0 +1,118 @@
+import os
+import rospkg
+import rospy
+from python_qt_binding import *
+from PyQt5 import *
+from dynamixel_workbench_msgs.srv import *
+from dynamixel_workbench_msgs.msg import *
+
+
+
+
+
+
+class SensorHeadHMIWidget(QtWidgets.QWidget):
+    def __init__(self):
+
+        # Start the HMI
+        super(SensorHeadHMIWidget, self).__init__()
+        ui_file = os.path.join(rospkg.RosPack().get_path('tracking_cam_gui'), 'resource', 'sensor_head_hmi.ui')
+        loadUi(ui_file, self)
+
+
+        # SEND INFO
+        self.axis_1_position.valueChanged[int].connect(self.change_motor_position)
+        self.axis_2_position.valueChanged[int].connect(self.change_motor_position2)
+        self.axis_1_position_2.valueChanged[int].connect(self.change_motor_position3)
+        self.enable_motor.clicked[bool].connect(self.ChangeMotorState1)
+
+
+        # RECEIVE INFO
+        self.motor_sub = rospy.Subscriber("/dynamixel_workbench/dynamixel_state", DynamixelStateList, self.UpdateMotorsData)
+
+
+
+
+
+
+    def UpdateMotorsData(self, state):
+        '''
+        Update the motor dictionnary information with the dynamixel state list
+        '''
+        self.motors_data = {}
+        for motor in state.dynamixel_state:
+            self.motors_data[motor.id] = motor.present_position
+            self.actual_pos_axis1.setNum(self.motors_data[3])
+
+
+
+    def change_motor_position(self, desired_position):
+        rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
+        try:
+            move_motor = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 3
+            request.addr_name = "Goal_Position"
+            request.value = desired_position
+            response = move_motor(request)
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            print("Error here")
+
+
+    def change_motor_position2(self, desired_position):
+        rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
+        try:
+            move_motor = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 2
+            request.addr_name = "Goal_Position"
+            request.value = desired_position
+            response = move_motor(request)
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            print("Error here")
+
+    def change_motor_position3(self, desired_position):
+        rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
+        try:
+            move_motor = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 1
+            request.addr_name = "Goal_Position"
+            request.value = desired_position
+            response = move_motor(request)
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            print("Error here")
+
+
+
+    def ChangeMotorState1(self, desired_state):
+        '''
+        Will be depreceated
+        '''
+        self.ControlMotor(3, "Torque_Enable", desired_state)
+        pass
+
+
+
+
+    def ControlMotor(self, id, command, value):
+        '''
+        TODO: Will become the function used in independant ROS package
+        '''
+        rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
+        try:
+            service_object = rospy.ServiceProxy('/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 3
+            request.addr_name = command
+            request.value = value
+            service_call_response = service_object(request)
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
