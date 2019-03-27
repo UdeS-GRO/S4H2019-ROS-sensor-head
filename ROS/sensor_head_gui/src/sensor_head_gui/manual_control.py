bd0f7bf8c78865ad2753da4051797fcc933df8b8
Jonathan Delisle <jonathan.delisle@usherbrooke.ca>
2019-03-27 14:37:24 -0400

    Changed Manual_control class

--- ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
@@ -1,25 +1,32 @@
 import rospy
 from geometry_msgs.msg import Twist
+from 
 from sensor_msgs.msg import Joy
 
-class ManualControl(publisher):
+class ManualControl():
 	
-	def callback(data):
+	def callback(self, data):
 		twist = Twist()
 		twist.linear.x = 100*data.axes[0]
 		twist.linear.y = 100*data.axes[1]
 		twist.linear.z = 100*data.axes[3]
-		deadman = 100*data.axes[2]
-		pub.publish(twist)
-		pub2.publish(deadman)
+		#deadman = 100*data.axes[2]
+		self.pub.publish(twist)
+		#self.pub2.publish(deadman)
 
-	def _init_():
-		global pub
-		global pub2
-		pub = rospy.Publisher(publisher),Twist)
-		pub2 = rospy.Publisher(publisher),deadman)
-		rospy.Subscriber("joy", Joy, callback)
+	def __init__(self, publisher):
+
+		
+		self.pub = rospy.Publisher(publisher, Twist)
+		#self.pub2 = rospy.Publisher(publisher, deadman)
+		rospy.Subscriber("joy", Joy, self.callback)
 		rospy.spin()
 
-	if __name__ = '__main__'):
-		_init_()
+
+if __name__ == '__main__':
+    rospy.init_node('Joy2Turtle')
+    mc = ManualControl("MC_pub")
+    rospy.spin()
+	
+		
+		
