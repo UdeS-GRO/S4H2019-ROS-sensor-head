d54fdc25ee9f6197b608fe1e2e4a1c0b16702a2c
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2019-03-27 23:20:58 -0400

    Adjusted styling of the webpage, and image sent via roslibjs
    Corrected the styling of the web page and removed the old web_video_server dependency. Cleaned and commented code, and added images and icons (personally made)

--- ROS/sensor_head_gui/src/sensor_head_web_server/www/js/sensor_head_web_orientation/sensor_head_web_orientation.js
+++ ROS/sensor_head_gui/src/sensor_head_web_server/www/js/sensor_head_web_orientation/sensor_head_web_orientation.js
@@ -8,35 +8,6 @@ var beta, vbeta, x;
 var gamma, vgamma, y;
 
 var orientationMotionTimer;
-// Set up a global variable for the term div:
-// var term = document.getElementById('term');
-
-/**
- * @description Function to return a formatted date and time string
- * @author Edouard Choinière
- * @date 2019-03-07
- * @returns {string} The formatted date and time string
- */
-function getDateTimeString() {
-    const event = new Date();
-    const options = {
-        timeZoneName: 'short'
-    };
-    return event.toLocaleString(options);
-}
-
-/**
- * @description Function to log to the fake console
- * @author Edouard Choinière
- * @date 2019-03-07
- * @param {string} consoleLineString The string to log to the fake console
- */
-function termLog(consoleLineString) {
-    // term.innerHTML += "[" + getDateTimeString() + "] " + consoleLineString + "<br/>";
-    // term.scrollTop = term.scrollHeight;
-    console.log(consoleLineString);
-}
-
 
 
 // setup connection to the ROS server and prepare the topic
@@ -44,19 +15,17 @@ var ros = new ROSLIB.Ros();
 
 ros.on('connection', function () {
     console.log('Connected to websocket server.');
-    termLog('Connected to websocket server.');
 });
 
 ros.on('error', function (error) {
     console.log('Error connecting to websocket server: ', JSON.stringify(error));
     console.log(error);
-    termLog('Error connecting to websocket server: ' + JSON.stringify(error));
-    window.alert('Error connecting to websocket server');
+    window.alert('Error connecting to websocket server. Are you using an unsupported iOS device? Did you try on a an Android cellphone?');
 });
 
 ros.on('close', function () {
     console.log('Connection to websocket server closed.');
-    termLog('Connection to websocket server closed.');
+    
 });
 
 var imuTopic = new ROSLIB.Topic({
@@ -77,10 +46,10 @@ if (window.DeviceOrientationEvent) {
 
     // Listen for the deviceorientation event and handle the raw data
     window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-    termLog("Device orientation (angle) supported!");
+    colole.log("Device orientation (angle) supported!");
 
 } else {
-    termLog("Device orientation (angle) not supported!");
+    console.log("Device orientation (angle) not supported!");
     window.alert("Device orientation (angle) not supported!");
 }
 
@@ -107,10 +76,10 @@ function deviceOrientationHandler(eventData) {
 // setup event handler to capture the acceleration event and store the most recent data in a variable
 if (window.DeviceMotionEvent) {
     window.addEventListener('devicemotion', deviceMotionHandler, false);
-    termLog("Device motion (acceleration and rotation rate) supported!");
+    console.log("Device motion (acceleration and rotation rate) supported!");
 } else {
     termLog("Device motion (acceleration and rotation rate) not supported!");
-    window.alert("Device motion (acceleration and rotation rate) not supported!");
+    console.log("Device motion (acceleration and rotation rate) not supported!");
 }
 
 /**
@@ -132,7 +101,6 @@ function deviceMotionHandler(eventData) {
     vgamma = rotation.gamma;
     vbeta = rotation.beta;
     valpha = rotation.alpha;
-    // console.log(eventData);
 }
 
 function logOrientation(alpha_angle, beta_angle, gamma_angle) {
@@ -141,7 +109,7 @@ function logOrientation(alpha_angle, beta_angle, gamma_angle) {
         beta: beta_angle ? beta_angle.toFixed(3) : 0,
         gamma: gamma_angle ? gamma_angle.toFixed(3) : 0
     };
-    termLog(JSON.stringify(orientation));
+    console.log(orientation);
 }
 
 
@@ -156,8 +124,8 @@ function logMotion(accX, accY, accZ, v_alpha, v_beta, v_gamma) {
         vbeta: v_beta ? v_beta.toFixed(3) : 0,
         vgamma: v_gamma ? v_gamma.toFixed(3) : 0
     };
-    termLog(JSON.stringify(acceleration));
-    termLog(JSON.stringify(rotationRate));
+    console.log(acceleration);
+    console.log(rotationRate);
 }
 
 function publishImuSnapshot() {
@@ -230,18 +198,23 @@ function publishImuSnapshot() {
 // Need to use wss (secure websocket) instead of ws (websocket) since the
 // webpage is using https and all requests must be secured. Or else, the
 // browsers block the connection to the rosbridge server.
-termLog("wss://" + window.location.hostname + ":9090");
+console.log("wss://" + window.location.hostname + ":9090");
 ros.connect("wss://" + window.location.hostname + ":9090");
-// Logs the orientation and motion every 500 milliseconds
+
+
+// Captures and publishes the orientation and motion data every x milliseconds, 
+// where x is the number located after the anonymous function given for the 
+// setInterval argument.
 orientationMotionTimer = setInterval(function () {
     // logOrientation(alpha, beta, gamma);
     publishImuSnapshot();
     // logMotion(x, y, z, valpha, vbeta, vgamma);
 }, 75);
 
+// Subscribes to the image topic (what we call the video), and updates the src 
+// attribute at each message received.
 imageTopic.subscribe(function (message) {
     var imagedata = "data:image/jpeg;base64," + message.data;
-    // document.getElementById('camera_img').setAttribute('src', imagedata);
-    document.getElementById('camera_img').style.backgroundImage = "url('" +imagedata+"')";
-    // imageTopic.unsubscribe();
+    document.getElementById('camera_img').setAttribute('src', imagedata);
+    // imageTopic.unsubscribe(); // Uncomment to immediately unsubscribe after the first image. 
 });
