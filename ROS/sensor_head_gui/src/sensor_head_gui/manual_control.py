8292168b54b715922d5d1ddb000928b846060423
Jonathan Delisle <jonathan.delisle@usherbrooke.ca>
2019-04-04 11:14:15 -0400

    Updated manual_control + msg

--- ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
@@ -1,27 +1,30 @@
 import rospy
-from sensor_head_gui.msg import Controller
-from sensor_head_gui.msg import Deadman
+from sensor_head_gui.msg import X_Controller
 from sensor_msgs.msg import Joy
 
 class ManualControl():
 	
 	def callback(self, data):
-		controller = Controller()
-		deadman = Deadman()
-		controller.axis.x = 100*data.axes[0]
-		controller.axis.y = 100*data.axes[1]
-		controller.axis.z = 100*data.axes[3]
-		deadman.switch = 100*data.axes[5]
-		self.pub_axis.publish(controller)
-		self.pub_deadman.publish(deadman)
+		Xbox = X_Controller()
+		vitesse = 1 #degrés
+		
+		x_pos = x_pos + vitesse*data.axes[0]/32767
+		y_pos = y_pos + vitesse*data.axes[1]/32767
+		z_pos = z_pos + vitesse*data.axes[3]/32767
+		Xbox.axis.x = x_pos
+		Xbox.axis.y = y_pos
+		Xbox.axis.z = z_pos
+		
+		if(data.axes[5]>0)
+		    Xbox.deadman = 1
+		else
+		    Xbox.deadman = 0
+		self.pub_Xbox.publish(Xbox)
 
 	def __init__(self, publisher):
 
-		
-		self.pub_axis = rospy.Publisher(publisher, Controller, queue_size=10)
-		self.pub_deadman = rospy.Publisher(publisher, Deadman, queue_size=10)
-		rospy.Subscriber("joy", Joy, self.callback)
-		rospy.spin()
+		self.pubController = rospy.Publisher(publisher, X_Controller, queue_size=10)
+		self.subJoy = rospy.Subscriber("joy", Joy, self.callback)
 
 
 if __name__ == '__main__':
