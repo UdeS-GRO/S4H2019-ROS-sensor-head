ed52beafff1e6d637611f191f0ab72f155737ee6
Jonathan Delisle <jonathan.delisle@usherbrooke.ca>
2019-04-04 16:32:26 -0400

    Updated manual_control

--- ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
@@ -6,20 +6,30 @@ from sensor_msgs.msg import Joy
 
 class ManualControl():
     def callback(self, data):
+    
         Xbox = X_Controller()
         vitesse = 1  # degrees # TODO: To be specified in parameter
+        deadzone = 0.1
 
-        x_pos = x_pos + vitesse*data.axes[0]/32767
-        y_pos = y_pos + vitesse*data.axes[1]/32767
-        z_pos = z_pos + vitesse*data.axes[3]/32767
+        if (data.axes[0]>deadzone || data.axes[0]<-deadzone)
+            x_pos = x_pos + vitesse*data.axes[0]
+        if (data.axes[1]>deadzone || data.axes[1]<-deadzone)
+            y_pos = y_pos + vitesse*data.axes[1]
+        if (data.axes[3]>deadzone || data.axes[3]<-deadzone)
+            z_pos = z_pos + vitesse*data.axes[3]
+        if (data.buttons[6]==1)
+            x_pos = 0
+            y_pos = 0
+            z_pos = 0
         Xbox.axis.x = x_pos
         Xbox.axis.y = y_pos
         Xbox.axis.z = z_pos
 
-        if(data.axes[5] > 0):
+        if(data.axes[3] < 0 || data.axes[5] < 0):
             Xbox.deadman = 1
         else:
             Xbox.deadman = 0
+            
         self.pub_Xbox.publish(Xbox)
 
     def __init__(self, publisher):
