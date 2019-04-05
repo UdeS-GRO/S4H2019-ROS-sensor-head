1a4f1634d385793c07b5c3f979bd2d0e267e86a2
Jonathan Delisle <jonathan.delisle@usherbrooke.ca>
2019-04-05 13:52:35 -0400

    main_control update

--- ROS/sensor_head_gui/src/sensor_head_gui/main_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/main_control.py
@@ -11,17 +11,60 @@ from rqt_gui.main import Main
 from std_msgs.msg import String
 import sys
 from std_msgs.msg import Int32
+from sensor_head_gui.msg import X_Controller
 
 #def callback(data):
 #     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
-     
-def listener():
-     rospy.init_node('listener', anonymous=True)
- 
-     rospy.Subscriber("Xbox",10)
- 
-     # spin() simply keeps python from exiting until this node is stopped
-     rospy.spin()
+class main_control(self):
+     def __init__(self, publisher):
 
+          self.subJoy = rospy.Subscriber("Xbox", X_Controller, self.ControlMotor)
+          self.motor_sub = rospy.Subscriber(
+            "/dynamixel_workbench/dynamixel_state", DynamixelStateList, self.UpdateMotorsData)
+
+     def change_motor_positionmotor1(self, data):
+        """Sends a request to change the desired position of a motor specified by its ID.
+
+        Arguments:
+            motor_id {int} -- ID of the motor. Must match the dynamixel motor's id.
+            desired_position {[type]} -- [description]
+        """
+
+        rospy.wait_for_service('/dynamixel_workbench/dynamixel_command')
+        try:
+          ## motor z
+            move_motor = rospy.ServiceProxy(
+                '/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 1
+            request.addr_name = "Goal_Position"
+            request.value = data.axis.z
+            response = move_motor(request)
+
+          ## motor x
+            move_motor = rospy.ServiceProxy(
+                '/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 2
+            request.addr_name = "Goal_Position"
+            request.value = data.axis.x
+            response = move_motor(request)
+
+          ## motor y
+          move_motor = rospy.ServiceProxy(
+                '/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+            request = DynamixelCommandRequest()
+            request.id = 3
+            request.addr_name = "Goal_Position"
+            request.value = data.axis.y
+            response = move_motor(request)
+
+
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" % e
+            print("Error here")
+            
+
+     
 if __name__ == '__main__':
           listener()
--- ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
@@ -4,7 +4,7 @@ from sensor_head_gui.msg import X_Controller
 from sensor_msgs.msg import Joy
 
 
-class ManualControl():
+class manual_control():
     def callback(self, data):
 
         Xbox = X_Controller()
@@ -32,10 +32,7 @@ class ManualControl():
 
         self.pub_Xbox.publish(Xbox)
 
-    def __init__(self, publisher):
-
-        self.pub_Xbox = rospy.Publisher(publisher, X_Controller, queue_size=10)
-        self.subJoy = rospy.Subscriber("joy", Joy, self.callback)
+   
     
     def __init__(self):
         self.x_pos = 0
@@ -56,33 +53,3 @@ if __name__ == '__main__':
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
-
-# def talkerXbox(data):
-#     Xbox = X_Controller()
-#     vitesse = 1  # degrees # TODO: To be specified in parameter
-#     deadzone = 0.1
-
-#     if(data.axes[2] < 0 or data.axes[5] < 0):
-#         Xbox.deadman = 1
-#         if (data.axes[0] > deadzone or data.axes[0] < -deadzone):
-#             x_pos = x_pos + vitesse*data.axes[0]
-#         if (data.axes[1] > deadzone or data.axes[1] < -deadzone):
-#             y_pos = y_pos + vitesse*data.axes[1]
-#         if (data.axes[3] > deadzone or data.axes[3] < -deadzone):
-#             z_pos = z_pos + vitesse*data.axes[3]
-#         if (data.buttons[6] == 1):
-#             x_pos = 0
-#             y_pos = 0
-#             z_pos = 0
-#     else:
-#         Xbox.deadman = 0
-
-#     Xbox.axis.x = x_pos
-#     Xbox.axis.y = y_pos
-#     Xbox.axis.z = z_pos
-
-#     # if(Xbox.deadman==1):
-
-#     # rospy.loginfo(Xbox)
-#     pub_Xbox.publish(Xbox)
-#     # rate.sleep()
