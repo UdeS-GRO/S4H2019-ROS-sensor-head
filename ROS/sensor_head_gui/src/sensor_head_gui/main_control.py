a5de7e378a2a1be2fcc25b82c6c82799a228fd2e
Jonathan Delisle <jonathan.delisle@usherbrooke.ca>
2019-04-05 12:43:12 -0400

    manual_control node functional

--- ROS/sensor_head_gui/src/sensor_head_gui/main_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/main_control.py
@@ -3,7 +3,7 @@ import rospkg
 import rospy
 from python_qt_binding import *
 from PyQt5 import *
-from PyQt5.QtCore import pyqtSlot
+#from PyQt5.QtCore import pyqtSlot
 from dynamixel_workbench_msgs.srv import *
 from dynamixel_workbench_msgs.msg import *
 from functools import partial
@@ -18,18 +18,10 @@ from std_msgs.msg import Int32
 def listener():
      rospy.init_node('listener', anonymous=True)
  
-     rospy.Subscriber("Xbox",1)
+     rospy.Subscriber("Xbox",10)
  
      # spin() simply keeps python from exiting until this node is stopped
      rospy.spin()
-  
-def __init__(self):
-    self.pub = rospy.Publisher('chatter', String, queue_size=10)
-    self.rate = rospy.Rate(10)  # 10hz
 
-def refresh(self):
-    # while not rospy.is_shutdown():
-    hello_str = "hello world %s" % rospy.get_time()
-    rospy.loginfo(hello_str)
-    self.pub.publish(hello_str)
-    self.rate.sleep()
+if __name__ == '__main__':
+          listener()
--- ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
@@ -3,50 +3,86 @@ import rospy
 from sensor_head_gui.msg import X_Controller
 from sensor_msgs.msg import Joy
 
-def talker():
 
-    Xbox = X_Controller()
-    vitesse = 1  # degrees # TODO: To be specified in parameter
-    deadzone = 0.1
-    x_pos = 0
-    y_pos = 0
-    z_pos = 0
-    
-    pub_Xbox = rospy.Publisher('Xbox', X_Controller, queue_size=10)
-    subJoy = rospy.Subscriber("joy", Joy, talker)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    
-    while not rospy.is_shutdown():
-        if (data.axes[0]>deadzone || data.axes[0]<-deadzone):
-            x_pos = x_pos + vitesse*data.axes[0]
-        if (data.axes[1]>deadzone || data.axes[1]<-deadzone):
-            y_pos = y_pos + vitesse*data.axes[1]
-        if (data.axes[3]>deadzone || data.axes[3]<-deadzone):
-            z_pos = z_pos + vitesse*data.axes[3]
-        if (data.buttons[6]==1):
-            x_pos = 0
-            y_pos = 0
-            z_pos = 0
-        Xbox.axis.x = x_pos
-        Xbox.axis.y = y_pos
-        Xbox.axis.z = z_pos
+class ManualControl():
+    def callback(self, data):
 
-        if(data.axes[3] < 0 || data.axes[5] < 0):
+        Xbox = X_Controller()
+        vitesse = 1  # degrees # TODO: To be specified in parameter
+        deadzone = 0.1
+
+        if(data.axes[2] < 0 or data.axes[5] < 0):
             Xbox.deadman = 1
+            if (data.axes[0] > deadzone or data.axes[0] < -deadzone):
+                self.x_pos = self.x_pos + vitesse*data.axes[0]
+            if (data.axes[1] > deadzone or data.axes[1] < -deadzone):
+                self.y_pos = self.y_pos + vitesse*data.axes[1]
+            if (data.axes[3] > deadzone or data.axes[3] < -deadzone):
+                self.z_pos = self.z_pos + vitesse*data.axes[3]
+            if (data.buttons[6] == 1):
+                self.x_pos = 0
+                self.y_pos = 0
+                self.z_pos = 0
         else:
-             Xbox.deadman = 0
-        
-        #rospy.loginfo(Xbox)        
-        pub_Xbox.publish(Xbox)
-        rate.sleep()
+            Xbox.deadman = 0
+
+        Xbox.axis.x = self.x_pos
+        Xbox.axis.y = self.y_pos
+        Xbox.axis.z = self.z_pos
+
+        self.pub_Xbox.publish(Xbox)
+
+    def __init__(self, publisher):
+
+        self.pub_Xbox = rospy.Publisher(publisher, X_Controller, queue_size=10)
+        self.subJoy = rospy.Subscriber("joy", Joy, self.callback)
+    
+    def __init__(self):
+        self.x_pos = 0
+        self.y_pos = 0
+        self.z_pos = 0
+
+        self.pub_Xbox = rospy.Publisher('Xbox', X_Controller, queue_size=10)
+        self.subJoy = rospy.Subscriber("joy", Joy, self.callback)
 
 
 if __name__ == '__main__':
     try:
-         talker()
+        rospy.init_node('talker', anonymous=True)
+        mc = ManualControl()
+
+        # pub_Xbox = rospy.Publisher('Xbox', X_Controller, queue_size=10)
+        # subJoy = rospy.Subscriber("joy", Joy, talkerXbox)
+        rospy.spin()
     except rospy.ROSInterruptException:
-         pass
-         
-         
-         
+        pass
+
+# def talkerXbox(data):
+#     Xbox = X_Controller()
+#     vitesse = 1  # degrees # TODO: To be specified in parameter
+#     deadzone = 0.1
+
+#     if(data.axes[2] < 0 or data.axes[5] < 0):
+#         Xbox.deadman = 1
+#         if (data.axes[0] > deadzone or data.axes[0] < -deadzone):
+#             x_pos = x_pos + vitesse*data.axes[0]
+#         if (data.axes[1] > deadzone or data.axes[1] < -deadzone):
+#             y_pos = y_pos + vitesse*data.axes[1]
+#         if (data.axes[3] > deadzone or data.axes[3] < -deadzone):
+#             z_pos = z_pos + vitesse*data.axes[3]
+#         if (data.buttons[6] == 1):
+#             x_pos = 0
+#             y_pos = 0
+#             z_pos = 0
+#     else:
+#         Xbox.deadman = 0
+
+#     Xbox.axis.x = x_pos
+#     Xbox.axis.y = y_pos
+#     Xbox.axis.z = z_pos
+
+#     # if(Xbox.deadman==1):
+
+#     # rospy.loginfo(Xbox)
+#     pub_Xbox.publish(Xbox)
+#     # rate.sleep()
