61c9e5ce02e37a51da549a1f64518c73264a7946
Jonathan Delisle <jonathan.delisle@usherbrooke.ca>
2019-04-09 18:45:41 -0400

    data type joy

--- ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
+++ ROS/sensor_head_gui/src/sensor_head_gui/manual_control.py
@@ -64,7 +64,9 @@ class manual_control():
     def __init__(self):
         """[summary]
         """
-        self.currentData = X_Controller()
+        self.currentData = Joy()
+        self.currentData.axes=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.currentData.buttons=[0,0,0,0,0,0,0,0,0,0,0]
         self.z_pos = setHome[0]
         self.x_pos = setHome[1]
         self.y_pos = setHome[2]
